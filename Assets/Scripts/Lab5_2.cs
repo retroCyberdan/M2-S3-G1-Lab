2c18a13854ef8d338fd2b50be8487635f7cc9e78
retroCyberdan <105318240+retroCyberdan@users.noreply.github.com>
2025-05-02 16:46:20 +0200

    Adding exercise 2

--- Assets/Scripts/Lab5_2.cs
+++ Assets/Scripts/Lab5_2.cs
@@ -1,18 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Lab5_2 : MonoBehaviour
 {
+    //dichiaro delle variabili intere esposte
+    public int dimensioneArray;
+    public int rangeMin;
+    public int rangeMax;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        int[] array = GeneraArray(dimensioneArray, rangeMin, rangeMax);
+        Debug.Log("Ecco tutti i valori presenti nel tuo Array:");
+        StampaArray(array);
+        int minimo = TrovaMinimo(array);
+        Debug.Log("Di tutti i valori, questo è il più piccolo --> " + minimo);
+        int massimo = TrovaMassimo(array);
+        Debug.Log("Questo, invece, è il valore più alto --> " + massimo);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //Creo l funzione 'GeneraArray'
+    int[] GeneraArray(int dimensione, int min, int max)
+    {
+        int[] array = new int[dimensione];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = Random.Range(min, max + 1);
+        }
+        return array;
+    }
+
+    //Creo la funzione'StampaArray'
+    void StampaArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log(array[i]);
+        }
+    }
+
+    //Creo la funzione 'TrovaMinimo'
+    int TrovaMinimo(int[] array)
+    {
+        int minimo = array[0];
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] < minimo)
+            {
+                minimo = array[i];
+            }
+        }
+        return minimo;
+    }
+
+    //Creo la funzione 'TrovaMassimo'
+    int TrovaMassimo(int[] array)
+    {
+        int massimo = array[0];
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] > massimo)
+            {
+                massimo = array[i];
+            }
+        }
+        return massimo;
     }
 }
